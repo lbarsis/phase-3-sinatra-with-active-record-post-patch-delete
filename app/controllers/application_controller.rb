77bd2f8c9a028e3c708e537b3247bec7eb233a5f
Levi Barsis <lbarsis@gmail.com>
2022-12-27 13:31:33 -0800

    done with assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  delete '/reviews/:id' do 
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
@@ -14,6 +25,26 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  post '/reviews' do
+    # binding.pry
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    new_review = Review.find(params[:id])
+    new_review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    new_review.to_json
   end
 
 end
